4443e2ea0c0930922580954dee156cc1f3ea05a5
Alex C-G <alexcg@outlook.com>
2020-07-28 15:48:07 +1200

    strip out junk, add messages

--- process_data.py
+++ process_data.py
@@ -9,6 +9,8 @@ with open(input_file, 'r') as file:
 
 # print(data[series])
 
+print(f"Processing series {series}")
+
 data = data[series]
 all_lines = []
 separator = '!'
@@ -17,17 +19,14 @@ for episode in data:
     this_ep = data[episode]
     for key, values in this_ep.items():
         if len(values) > 0:
-            # print(f"{key}: {values}")
             for value in values:
                 line = (f"{key}{separator}{value}")
                 all_lines.append(line)
 
-print(all_lines)
-
-for line in all_lines:
-    print(line)
-
+print(f"Writing to {output_file}")
 with open(output_file, 'w') as file:
     for line in all_lines:
         file.write(line)
         file.write('\n')
+
+print("Done")
